cc095d3e5062391d3c4844df32d7a2340409b573
hechentao <hechentao@chinahddz.com>
2022-07-05 17:42:34 +0800

    开发任务 #109797 电子签名优化-移动端form-mobile同步支持

--- src/display/form/form-item/mobile/signature.vue
+++ src/display/form/form-item/mobile/signature.vue
@@ -1,37 +1,70 @@
 <template>
-    <div class="dsp__signature">
-        <mue-signature v-model="imgs" readonly></mue-signature>
-    </div>
+  <div class="dsp-signature">
+    <template v-if="signImgs.length">
+      <div class="sign-image-box" v-for="(img, i) in signImgs" :key="i">
+        <img class="img-url" :src="img"  style="object-fit: cover;" @click="previewImg(i)"/>
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
-    import {strToArr} from '../../../../utils/lib'
-
-    export default {
-        name: "DspSignature",
-        components: {},
-        props: ["value"],
-        data(){
-            return {};
-        },
-        computed: {
-            imgs: {
-                get() {
-                    if(Array.isArray(this.value)) return this.value;
-                    return strToArr(this.value)
-                },
-                set() {
-
-                }
-            }
-        },
-        methods: {}
+import Vue from "vue"
+import { ImagePreview } from 'vant';
+Vue.use(ImagePreview);
+export default {
+  name: "DspSignature",
+  components: {},
+  props: ["value", "model", "field"],
+  data(){
+    return {};
+  },
+  computed: {
+    signImgs() {
+      let v = this.model[this.field] || "";
+      return v ? v.split(",") : [];
     }
+  },
+  methods: {
+    previewImg(i){
+      ImagePreview({
+        images: this.signImgs,
+        startPosition: i,
+      });
+    }
+  }
+}
 </script>
 
-<style lang="less">
-    .dsp__signature {
-        width: 100%;
-        box-sizing: border-box;
+<style lang="less" scoped>
+.dsp-signature {
+  width: 100%;
+  display: flex;
+  align-items: end;
+  justify-content: flex-end;
+  flex-flow: column wrap;
+
+  /deep/ .sign-image-box {
+    display: inline-flex;
+    align-items: center;
+    position: relative;
+    padding: 2px 0;
+    z-index: 123;
+
+    .img-url{
+      flex-grow: 0;
+      flex-shrink: 0;
+      flex-basis: auto;
+      display: inline-flex;
+      margin-right: 6px;
+      cursor: pointer;
+      width: 100px;
+      height: 28px;
+      border: 1px solid #d0d0d0;
+      align-items: center;
+      justify-content: center;
+      box-sizing: border-box;
     }
+  }
+}
 </style>
