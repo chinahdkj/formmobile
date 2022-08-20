0de789352d62e0df6e94b609294dc58d9d82f43e
hechentao <hechentao@chinahddz.com>
2022-08-20 18:47:26 +0800

    开发任务 #115894 APP：公司督查/现场人员督察/监理检查---地图显示问题

--- src/display/form/form-item/super-form-item/dialog.vue
+++ src/display/form/form-item/super-form-item/dialog.vue
@@ -1,17 +1,30 @@
 <template>
-    <div style="width:100%">
-        <!-- <van-button :style="btnStyle" size="small" :disabled="true" native-type="button">{{ btnName }}</van-button> -->
-        <div class="mue-select">
-            <div class="mue-form-input has-suffix"
-                :class="{'mue-form-input__is-disabled': !!disabled}">
-                <input type="text" class="input__inner" readonly :value="btnName" :disabled="!!disabled"
-                    :placeholder="btnName" onfocus="this.blur()"/>
-                <slot name="icon">
-                    <i class="input__suffix input__suffix_icon iconfont icon-arrows-copy-copy"></i>
-                </slot>
-            </div>
-        </div>        
+  <div style="width:100%">
+    <!-- <van-button :style="btnStyle" size="small" :disabled="!!disabled" native-type="button"
+               @click="dialog.visible = true">{{ btnName }}</van-button> -->
+    <div class="mue-select">
+      <div class="mue-form-input has-suffix" @click="openPopup"
+           :class="{'mue-form-input__is-disabled': !displayReadonly}">
+        {{btnName}}
+        <slot name="icon">
+          <i class="input__suffix input__suffix_icon iconfont icon-202"></i>
+        </slot>
+      </div>
     </div>
+    <van-popup v-model="dialog.visible" get-container="body" :close-on-click-overlay="true"
+               :class="dialogClass ? `form-custom-dialog ${dialogClass} ` : 'form-custom-dialog '"
+               position="right" style="width:90%;height:100%;">
+      <div class="form-custom-dialog-header">{{dialogTitle || btnName}}</div>
+      <div class="form-custom-dialog-container">
+        <iframe v-if="url && dialog.visible" frameborder="0" :src="targetUrl"
+                :style="{width: '100%', height: '100%'}"/>
+      </div>
+      <!-- <div class="form-custom-dialog-footer">
+          <van-button size="large" type="default" @click="dialog.visible = false">取消</van-button>
+          <van-button size="large" type="primary" >确定</van-button>
+      </div> -->
+    </van-popup>
+  </div>
 </template>
 
 <script>
@@ -22,14 +35,15 @@ export default {
     name: "DspDialog",
     inheritAttrs: false,
     components: {},
-    props: ["field", "model", "disabled", "btnName", "url", "fullscreen", "dialogWidth", "dialogHeight",
-        "dialogClass", "dialogTitle", "btnWidth"],
+    props: ["field", "model", "disabled", "btnName", "url", "mobileUrl", "fullscreen", "dialogWidth", "dialogHeight",
+        "dialogClass", "dialogTitle", "btnWidth", "rowIndex", "vars", "displayReadonly"],
     data() {
         return {
             dialog: {
                 visible: false
             },
-            postMessageModel: {}
+            postMessageModel: {},
+            pushPostMessageModel: {}
         };
     },
     computed: {
@@ -42,7 +56,29 @@ export default {
             }
         },
         targetUrl() {
-            return TransferUrl(this.url, this.model)
+            const path = location.origin + location.pathname
+            const sn = sessionStorage.getItem('app_sn') ? `${sessionStorage.getItem('app_sn')}` : '';
+            let pp = path.split(`/${sn}/`)[0]
+            pp = pp + (pp.endsWith('/') ? `${sn}/hddev` : `/${sn}/hddev`)
+            if(!sn){
+              pp = location.origin
+            }
+            let trans_url = this.mobileUrl || this.url
+            let url = TransferUrl(trans_url, this.model, this.vars)
+            // url = url.replace("$rowIndex", this.rowIndex)
+            //子表链接自动携带上rowIndex
+            if(this.rowIndex !== undefined) {
+              url = (url || "").includes("?") ? `${url}&rowIndex=${this.rowIndex}` : `${trans_url}?rowIndex=${this.rowIndex}`
+            }
+            if(!!this.displayReadonly) {
+              url = (url || "").includes("?") ? `${url}&isDetail=1` : `${trans_url}?isDetail=1`
+            }
+            const ur = new URL((url.startsWith('/') ? `${pp}${url}` : `${pp}/${url}`))
+            ur.searchParams.set('appid',sessionStorage.getItem('hddevappid') || sessionStorage.getItem('appid'))
+            ur.searchParams.set('token',sessionStorage.getItem('authortoken'))
+            ur.searchParams.set('app',sessionStorage.getItem('authorapp'))
+            sessionStorage.getItem('host') && ur.searchParams.set('host',sessionStorage.getItem('host'))
+            return this.checkURL(url) ? url : ur.href
         },
         btnStyle() {
             return this.btnWidth ? {width: this.btnWidth} : {}
@@ -60,41 +96,62 @@ export default {
             }
         }
     },
-    methods: {}
+    methods: {
+        openPopup(){
+            if(!this.displayReadonly) return
+            this.dialog.visible = true
+        },
+        checkURL(URL){
+            let str=URL;
+            let objExp=new RegExp(/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- .\/?%&=]*)?/);
+            if(objExp.test(str)==true){
+                return true;
+            }else{
+                return false;
+            }
+        }
+    }
 }
 </script>
 
 <style lang="less" scoped>
-    .form-custom-dialog{
-        &.fullscreen{
-            width: 100%;
-            height: 100%;
-            box-sizing: border-box;
-            padding: 0;
-        }
-        .form-custom-dialog-header{
+.mue-select{
+    .mue-form-input{
+        border: none;
+        padding: 0;
+        height: 30px;
+    }
+}
+.form-custom-dialog{
+    &.fullscreen{
+        width: 100%;
+        height: 100%;
+        box-sizing: border-box;
+        padding: 0;
+    }
+    .form-custom-dialog-header{
+          height: 40px;
+          line-height: 40px;
+          padding: 0 12px;
+          position: relative;
+          padding-right: 40px;
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        .form-custom-dialog-header-close{
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 40px;
             height: 40px;
-            line-height: 40px;
-            padding: 0 12px;
-            position: relative;
-            padding-right: 40px;
-            white-space: nowrap;
-            overflow: hidden;
-            text-overflow: ellipsis;
-            .form-custom-dialog-header-close{
-                position: absolute;
-                top: 0;
-                right: 0;
-                width: 40px;
-                height: 40px;
-                text-align: center;
-                line-height: 42px;
-            }
-        }
-        .form-custom-dialog-container{
-            height: calc(100% - 40px);
+            text-align: center;
+            line-height: 42px;
         }
     }
+    .form-custom-dialog-container{
+      height: calc(100% - 40px);
+    }
+}
 </style>
 <style lang="less">
 body .form-display .custom-form-group-grids .dsp-form-item:not(.default-dialog-btn) ,
